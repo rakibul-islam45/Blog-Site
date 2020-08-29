c049a9e41aec3c24cd9b5c394e532a93ee90bc50
rakibul-islam45 <zero.rakibul@gmail.com>
2020-08-30 04:11:29 +0600

    cinfig added

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$server = "127.0.0.1";
+$dbname = "blogSite";
+$user = "rakib";
+$pass = "rakibul";
+$pdo = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass);
+
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch (Throwable $t){
+    echo 'Cannot Connect to Database';
+    die;
+}
+
+       // var_dump($pdo->query("select version()"));
