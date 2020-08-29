f8941751aa5ee5b93f057d414533a60bbebcd47b
rakibul-islam45 <zero.rakibul@gmail.com>
2020-08-30 04:21:27 +0600

    register added

---
+++ registerfunction.php
@@ -0,0 +1,69 @@
+<?php
+    include ('./config.php');
+
+$username = "";
+$email    = "";
+$errors = array();
+
+
+if (isset($_POST['reg_user'])) {
+
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password_1 = $_POST['password_1'];
+    $password_2 = $_POST['password_2'];
+
+
+    if (empty($name)) {
+        array_push($errors, "Please insert your name");
+    }
+    if (empty($username)) {
+        array_push($errors, "Please insert your username");
+    }
+    if (empty($email)) {
+        array_push($errors, "Insert Email!");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Insert password");
+    }
+    if ($password_1 != $password_2) {
+        array_push($errors, "Passwords don't match");
+    }
+
+
+    $query = "SELECT * FROM users WHERE userName='$username' 
+								OR email='$email' LIMIT 1";
+
+    $result = mysqli_query($pdo, $query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) {
+        if ($user['userName'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+        if ($user['email'] === $email) {
+            array_push($errors, "Email already exists");
+        }
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password_1);
+        $query = "INSERT INTO users (name, userName, email, password) 
+					  VALUES(':name',':username', ':email', ':password')";
+
+
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':password', $password);
+
+        if($statement->execute()) {
+           // header("location: login.php");
+            echo 'Inserted';
+        } else {
+            echo 'Could not insert';
+        }
+    }
+}
