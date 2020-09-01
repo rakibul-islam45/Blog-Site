6338d56f4841e6a365a69e3404486eb433a4462b
rakibul-islam45 <zero.rakibul@gmail.com>
2020-09-02 03:52:51 +0600

    comment part.

---
+++ comments.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include ('config.php');
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
+//var_dump($username);
+if($userid) {
+    if(isset($_POST['comment_btn'])) {
+        $comment = $_POST['comment'];
+        $postid = $_POST['postid'];
+
+
+        $query = "INSERT INTO  comments (postid, userName, comment) values (:postid, :username, :comment)";
+
+        $statement = $pdo->prepare($query);
+
+        $statement->bindValue(':postid', $postid);
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':comment', $comment);
+
+        if ($statement->execute()) {
+//    header("location: login.php");
+            echo 'Inserted';
+        } else {
+            echo 'Could not insert';
+        }
+
+    }
+
+} else{
+    echo "<script type='text/javascript'>alert('Please register to comment!');</script>";
+    header('location : register.php');
+    }
+
