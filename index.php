14b364a6f228322c2673e89141c06b44a0a4fc94
rakibul-islam45 <zero.rakibul@gmail.com>
2020-09-03 04:07:57 +0600

    index.php updated.

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+    session_start();
     include_once ('config.php');
     $query = "SELECT * FROM post ORDER BY postId DESC ";
     $statement = $pdo->prepare($query);
     $statement->execute();
 
     $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: index.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,15 +62,22 @@
 
 			<div class="colorlib-footer">
 				<h1 id="colorlib-logo" class="mb-4"><a href="index.php" style="background-image: url(images/bg_1.jpg);">Blog <span>Site</span></a></h1>
-				<div class="mb-4">
-					<h3>Subscribe for newsletter</h3>
-					<form action="#" class="colorlib-subscribe-form">
-            <div class="form-group d-flex">
-            	<div class="icon"><span class="icon-paper-plane"></span></div>
-              <input type="text" class="form-control" placeholder="Enter Email Address">
-            </div>
-          </form>
-				</div>
+<!--				<div class="mb-4">-->
+<!--					<h3>Subscribe for newsletter</h3>-->
+<!--					<form action="#" class="colorlib-subscribe-form">-->
+<!--            <div class="form-group d-flex">-->
+<!--            	<div class="icon"><span class="icon-paper-plane"></span></div>-->
+<!--              <input type="text" class="form-control" placeholder="Enter Email Address">-->
+<!--            </div>-->
+<!--          </form>-->
+<!--				</div>-->
+                <?php  if (isset($_SESSION['username'])) : ?>
+                    <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+                    <p> <a href="index.php?logout='1'" style="color: red;" class="btn py-3 px-4 btn-primary" >Logout</a> </p>
+                <?php endif ?>
+                <?php  if (empty($_SESSION['username'])) : ?>
+                    <p><strong><a href="login.php" class="btn py-3 px-4 btn-primary">Login</a></strong> </p>
+                <?php endif ?>
 				<p class="pfooter"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
@@ -240,4 +256,4 @@
     
   </body>
 </html>
-echo "hello Rakibul";
+
