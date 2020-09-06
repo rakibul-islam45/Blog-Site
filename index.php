9dd68af18ef9a9b9088a3f670d2035e754dc7998
rakibul-islam45 <zero.rakibul@gmail.com>
2020-09-07 01:57:41 +0600

    index updated.

--- index.php
+++ index.php
@@ -62,17 +62,9 @@ if (isset($_GET['logout'])) {
 
 			<div class="colorlib-footer">
 				<h1 id="colorlib-logo" class="mb-4"><a href="index.php" style="background-image: url(images/bg_1.jpg);">Blog <span>Site</span></a></h1>
-<!--				<div class="mb-4">-->
-<!--					<h3>Subscribe for newsletter</h3>-->
-<!--					<form action="#" class="colorlib-subscribe-form">-->
-<!--            <div class="form-group d-flex">-->
-<!--            	<div class="icon"><span class="icon-paper-plane"></span></div>-->
-<!--              <input type="text" class="form-control" placeholder="Enter Email Address">-->
-<!--            </div>-->
-<!--          </form>-->
-<!--				</div>-->
+
                 <?php  if (isset($_SESSION['username'])) : ?>
-                    <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+                    <p>Welcome <strong><a href="userprofile.php"  style="color: #6f42c1" ><?php echo $_SESSION['username']; ?></a></strong></p>
                     <p> <a href="index.php?logout='1'" style="color: red;" class="btn py-3 px-4 btn-primary" >Logout</a> </p>
                 <?php endif ?>
                 <?php  if (empty($_SESSION['username'])) : ?>
